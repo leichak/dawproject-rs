0bea48375b249afe898d181b734ea1a13094c8ce
Leinad Roczak <daniel.kac94@gmail.com>
2024-05-20 23:27:31 +0200

    adding-test

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -86,12 +86,97 @@ pub fn reset_xml_id() {
     let _ = ID_XML.fetch_and(0, Ordering::SeqCst);
 }
 
+static XML: &str = r##"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Project version="1.0">
+<Application name="Bitwig Studio" version="5.0"/>
+<Transport>
+<Tempo max="666.000000" min="20.000000" unit="bpm" value="149.000000" id="id0" name="Tempo"/>
+<TimeSignature denominator="4" numerator="4" id="id1"/>
+</Transport>
+<Structure>
+<Track contentType="notes" loaded="true" id="id2" name="Bass" color="#a2eabf">
+  <Channel audioChannels="2" destination="id15" role="regular" solo="false" id="id3">
+    <Devices>
+      <ClapPlugin deviceID="org.surge-synth-team.surge-xt" deviceName="Surge XT" deviceRole="instrument" loaded="true" id="id7" name="Surge XT">
+        <Parameters/>
+        <Enabled value="true" id="id8" name="On/Off"/>
+        <State path="plugins/d19b1f6e-bbb6-42fe-a6c9-54b41d97a05d.clap-preset"/>
+      </ClapPlugin>
+    </Devices>
+    <Mute value="false" id="id6" name="Mute"/>
+    <Pan max="1.000000" min="0.000000" unit="normalized" value="0.500000" id="id5" name="Pan"/>
+    <Volume max="2.000000" min="0.000000" unit="linear" value="0.659140" id="id4" name="Volume"/>
+  </Channel>
+</Track>
+<Track contentType="audio" loaded="true" id="id9" name="Drumloop" color="#b53bba">
+  <Channel audioChannels="2" destination="id15" role="regular" solo="false" id="id10">
+    <Mute value="false" id="id13" name="Mute"/>
+    <Pan max="1.000000" min="0.000000" unit="normalized" value="0.500000" id="id12" name="Pan"/>
+    <Volume max="2.000000" min="0.000000" unit="linear" value="0.177125" id="id11" name="Volume"/>
+  </Channel>
+</Track>
+<Track contentType="audio notes" loaded="true" id="id14" name="Master">
+  <Channel audioChannels="2" role="master" solo="false" id="id15">
+    <Mute value="false" id="id18" name="Mute"/>
+    <Pan max="1.000000" min="0.000000" unit="normalized" value="0.500000" id="id17" name="Pan"/>
+    <Volume max="2.000000" min="0.000000" unit="linear" value="1.000000" id="id16" name="Volume"/>
+  </Channel>
+</Track>
+</Structure>
+<Arrangement id="id19">
+<Lanes timeUnit="beats" id="id20">
+  <Lanes track="id2" id="id21">
+    <Clips id="id22">
+      <Clip time="0.0" duration="8.0" playStart="0.0">
+        <Notes id="id23">
+          <Note time="0.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
+          <Note time="1.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
+          <Note time="4.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
+          <Note time="5.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
+          <Note time="0.500000" duration="0.250000" channel="0" key="64" vel="0.787402" rel="0.787402"/>
+          <Note time="4.500000" duration="0.250000" channel="0" key="64" vel="0.787402" rel="0.787402"/>
+          <Note time="1.500000" duration="2.500000" channel="0" key="53" vel="0.787402" rel="0.787402"/>
+          <Note time="5.500000" duration="0.250000" channel="0" key="53" vel="0.787402" rel="0.787402"/>
+          <Note time="6.000000" duration="2.000000" channel="0" key="53" vel="0.787402" rel="0.787402"/>
+        </Notes>
+      </Clip>
+    </Clips>
+  </Lanes>
+  <Lanes track="id9" id="id24">
+    <Clips id="id25">
+      <Clip time="0.0" duration="8.00003433227539" playStart="0.0" loopStart="0.0" loopEnd="8.00003433227539" fadeTimeUnit="beats" fadeInTime="0.0" fadeOutTime="0.0" name="Drumfunk3 170bpm">
+        <Clips id="id26">
+          <Clip time="0.0" duration="8.00003433227539" contentTimeUnit="beats" playStart="0.0" fadeTimeUnit="beats" fadeInTime="0.0" fadeOutTime="0.0">
+            <Warps contentTimeUnit="seconds" timeUnit="beats" id="id28">
+              <Audio algorithm="stretch" channels="2" duration="2.823541666666667" sampleRate="48000" id="id27">
+                <File path="audio/Drumfunk3 170bpm.wav"/>
+              </Audio>
+              <Warp time="0.0" contentTime="0.0"/>
+              <Warp time="8.00003433227539" contentTime="2.823541666666667"/>
+            </Warps>
+          </Clip>
+        </Clips>
+      </Clip>
+    </Clips>
+  </Lanes>
+  <Lanes track="id14" id="id29">
+    <Clips id="id30"/>
+  </Lanes>
+</Lanes>
+</Arrangement>
+<Scenes/>
+</Project>
+"##;
+
 #[cfg(test)]
 mod project_creator {
 
     use std::collections::HashMap;
 
     use std::{error::Error, path::Path};
+
+    use crate::api::XML;
 
     use super::{
         super::api::device_mods::{
@@ -154,90 +239,7 @@ mod project_creator {
     fn load_daw_project_test() {
         use {super::project::Project, quick_xml::de::from_str};
 
-        let xml = r##"
-    <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Project version="1.0">
-  <Application name="Bitwig Studio" version="5.0"/>
-  <Transport>
-    <Tempo max="666.000000" min="20.000000" unit="bpm" value="149.000000" id="id0" name="Tempo"/>
-    <TimeSignature denominator="4" numerator="4" id="id1"/>
-  </Transport>
-  <Structure>
-    <Track contentType="notes" loaded="true" id="id2" name="Bass" color="#a2eabf">
-      <Channel audioChannels="2" destination="id15" role="regular" solo="false" id="id3">
-        <Devices>
-          <ClapPlugin deviceID="org.surge-synth-team.surge-xt" deviceName="Surge XT" deviceRole="instrument" loaded="true" id="id7" name="Surge XT">
-            <Parameters/>
-            <Enabled value="true" id="id8" name="On/Off"/>
-            <State path="plugins/d19b1f6e-bbb6-42fe-a6c9-54b41d97a05d.clap-preset"/>
-          </ClapPlugin>
-        </Devices>
-        <Mute value="false" id="id6" name="Mute"/>
-        <Pan max="1.000000" min="0.000000" unit="normalized" value="0.500000" id="id5" name="Pan"/>
-        <Volume max="2.000000" min="0.000000" unit="linear" value="0.659140" id="id4" name="Volume"/>
-      </Channel>
-    </Track>
-    <Track contentType="audio" loaded="true" id="id9" name="Drumloop" color="#b53bba">
-      <Channel audioChannels="2" destination="id15" role="regular" solo="false" id="id10">
-        <Mute value="false" id="id13" name="Mute"/>
-        <Pan max="1.000000" min="0.000000" unit="normalized" value="0.500000" id="id12" name="Pan"/>
-        <Volume max="2.000000" min="0.000000" unit="linear" value="0.177125" id="id11" name="Volume"/>
-      </Channel>
-    </Track>
-    <Track contentType="audio notes" loaded="true" id="id14" name="Master">
-      <Channel audioChannels="2" role="master" solo="false" id="id15">
-        <Mute value="false" id="id18" name="Mute"/>
-        <Pan max="1.000000" min="0.000000" unit="normalized" value="0.500000" id="id17" name="Pan"/>
-        <Volume max="2.000000" min="0.000000" unit="linear" value="1.000000" id="id16" name="Volume"/>
-      </Channel>
-    </Track>
-  </Structure>
-  <Arrangement id="id19">
-    <Lanes timeUnit="beats" id="id20">
-      <Lanes track="id2" id="id21">
-        <Clips id="id22">
-          <Clip time="0.0" duration="8.0" playStart="0.0">
-            <Notes id="id23">
-              <Note time="0.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
-              <Note time="1.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
-              <Note time="4.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
-              <Note time="5.000000" duration="0.250000" channel="0" key="65" vel="0.787402" rel="0.787402"/>
-              <Note time="0.500000" duration="0.250000" channel="0" key="64" vel="0.787402" rel="0.787402"/>
-              <Note time="4.500000" duration="0.250000" channel="0" key="64" vel="0.787402" rel="0.787402"/>
-              <Note time="1.500000" duration="2.500000" channel="0" key="53" vel="0.787402" rel="0.787402"/>
-              <Note time="5.500000" duration="0.250000" channel="0" key="53" vel="0.787402" rel="0.787402"/>
-              <Note time="6.000000" duration="2.000000" channel="0" key="53" vel="0.787402" rel="0.787402"/>
-            </Notes>
-          </Clip>
-        </Clips>
-      </Lanes>
-      <Lanes track="id9" id="id24">
-        <Clips id="id25">
-          <Clip time="0.0" duration="8.00003433227539" playStart="0.0" loopStart="0.0" loopEnd="8.00003433227539" fadeTimeUnit="beats" fadeInTime="0.0" fadeOutTime="0.0" name="Drumfunk3 170bpm">
-            <Clips id="id26">
-              <Clip time="0.0" duration="8.00003433227539" contentTimeUnit="beats" playStart="0.0" fadeTimeUnit="beats" fadeInTime="0.0" fadeOutTime="0.0">
-                <Warps contentTimeUnit="seconds" timeUnit="beats" id="id28">
-                  <Audio algorithm="stretch" channels="2" duration="2.823541666666667" sampleRate="48000" id="id27">
-                    <File path="audio/Drumfunk3 170bpm.wav"/>
-                  </Audio>
-                  <Warp time="0.0" contentTime="0.0"/>
-                  <Warp time="8.00003433227539" contentTime="2.823541666666667"/>
-                </Warps>
-              </Clip>
-            </Clips>
-          </Clip>
-        </Clips>
-      </Lanes>
-      <Lanes track="id14" id="id29">
-        <Clips id="id30"/>
-      </Lanes>
-    </Lanes>
-  </Arrangement>
-  <Scenes/>
-</Project>
-    "##;
-
-        let obj: Project = from_str(xml).unwrap();
+        let obj: Project = from_str(XML).unwrap();
 
         println!("Deserialized object {:#?}", obj);
     }
@@ -931,6 +933,11 @@ mod project_creator {
         /*
         This test should load project exported with original dawproject in java, save and load again to compare results.
          */
+        use {super::project::Project, quick_xml::de::from_str};
+
+        let obj: Project = from_str(XML).unwrap();
+
+        println!("Deserialized object {:#?}", obj);
         Ok(())
     }
 }
